d175945a0dab546ab36e992dcdc1485891b935c4
John Atkins <johnatkins1999@yahoo.com>
2017-08-24 08:54:58 -1000

    web-app

---
+++ index.js
@@ -0,0 +1,20 @@
+// Grab elements, create settings, etc.
+var video = document.getElementById('video');
+
+// Get access to the camera!
+if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    // Not adding `{ audio: true }` since we only want video now
+    navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+        video.src = window.URL.createObjectURL(stream);
+        video.play();
+    });
+}
+
+var c = document.getElementById("drawingCanvas");
+var ctx = c.getContext("2d");
+ctx.moveTo(0,0);
+ctx.lineTo(640,480);
+ctx.stroke();
+ctx.moveTo(0,480);
+ctx.lineTo(640,0);
+ctx.stroke();
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const methodOverride= require('method-override');
+const session= require('express-session');
+const app = express();
+const RedisStore = require('connect-redis')(session);
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const PORT = process.env.PORT || 9000;
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(methodOverride('_method'));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.serializeUser(function(user, done){
+  done(null, user.id);
+});
+passport.deserializeUser(function(id, done){
+  return Users.findById(id)
+  //we will not have to worry about the users model as of now since we are using MONGO!
+    .then(user => done(null, user))
+    .catch(err => done(err));
+});
+passport.use(new LocalStrategy((username, password, done)=>{
+  //we will use facebook strategy here
+  }));
+app.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
+});
