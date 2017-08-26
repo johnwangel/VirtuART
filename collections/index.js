3f019336121c48b3240e90841d9e33633e80420f
Oksana Harris <oksana.harris@gmail.com>
2017-08-25 19:34:38 -1000

    more database stuff

--- collections/index.js
+++ collections/index.js
@@ -5,13 +5,16 @@ let artData = null;
 
 MongoClient.connect(mongoConnectionString, function(err, connectedDb) {
   // assert.equal(null, err);
-  console.log(`Successfully connected to {mongoConnectionString}`);
+  console.log(`Successfully connected to ${mongoConnectionString}`);
   mongoDb = connectedDb;
 
   artData = mongoDb.collection('artCollection');
+  console.log('this is our art data', artData);
+  console.log('art data no invoke', artData.find().toArray());
+
   // db.close(); // don't want this piece
 
-  artData.insertOne({hello: 'world'});
+  // artData.insertOne({images: ["../img/lolcat.jpg", "../img/sparky.jpg", "../img/add.png", "../img/keith.jpg", "../img/liz.jpg", "../img/../img/ian.gif", "../img/kristin.png", "../img/oksana.jpg", "../img/lolcat.jpg", "../img/sparky.jpg", "../img/add.png", "../img/keith.jpg", "../img/liz.jpg", "../img/ian.gif", "../img/kristin.png", "../img/lolcat.jpg", "../img/sparky.jpg", "../img/add.png", "../img/keith.jpg", "../img/liz.jpg", "../img/../img/ian.gif", "../img/kristin.png","../img/lolcat.jpg", "../img/sparky.jpg", "../img/add.png", "../img/keith.jpg", "../img/liz.jpg", "../img/ian.gif", "../img/kristin.png", "../img/oksana.jpg"]});
 
 });
 
