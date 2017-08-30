1bac31fc6a90a87b3a8768671a0ce8e684569d89
John Atkins <johnatkins1999@yahoo.com>
2017-08-30 09:57:12 -1000

    id

--- public/pages/selection/selection.controller.js
+++ public/pages/selection/selection.controller.js
@@ -5,24 +5,21 @@ myApp.controller('SelectionController', [
   '$scope',
   '$location',
   'UsersService',
-  'ToolkitService',
-  function($scope, $location, UsersService, ToolkitService) {
+  function($scope, $location, UsersService) {
     $scope.photoURLs;
 
     $scope.loadCanvas = function(e){
       let thisID = e.path[0].id;
+      localStorage.setItem('currentID', thisID);
         UsersService.checkTile(thisID)
         .then( response => {
           if (response == false ){
             alert('Sorry, that canvas is taken. Please select a different one.')
-
           } else {
-            ToolkitService.setCurrentId(response);
             $location.path('/toolkit');
           }
       })
     }
-
     return UsersService.getTiles()
     .then(allData => {
       $scope.photoURLs = allData.tiles;
