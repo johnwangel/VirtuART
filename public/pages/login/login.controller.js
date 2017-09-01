804a66a85aa583fcea6ce4c2ec6695d0c685a72d
Oksana Harris <oksana.harris@gmail.com>
2017-09-01 11:55:16 -1000

    menu fixes

--- public/pages/login/login.controller.js
+++ public/pages/login/login.controller.js
@@ -1,22 +1,23 @@
 angular.module('myApp')
 .controller('LoginController', ['$scope', '$rootScope', 'UsersService', '$window', function($scope, $rootScope, UsersService, $window){
-myApp.controller(
-  'LoginController', ['$scope', 'LoginService', function($scope, LoginService) {
   $scope.newLogin = { username: '', password: '' };
   $scope.LoginService =LoginService;
+
   $scope.Login = function() {
+
     var newLogin = {
       username: $scope.newLogin.username,
       password: $scope.newLogin.password
     };
-   LoginService.Login(newLogin);
+
+    LoginService.Login(newLogin);
     $scope.Login.username = '';
     $scope.Login.password = '';
 
-  $scope.user = {
-    username: '',
-    password: ''
-};
+    $scope.user = {
+      username: '',
+      password: ''
+    };
 
   };
 
@@ -31,13 +32,13 @@ myApp.controller(
     .then(user => {
       console.log('came back to controller from loginUser method', user);
       if(!user) {$scope.feedback = 'Not a valid user'}
-      else {
-        localStorage.loggedInUserId = user.id;
-        localStorage.loggedIn = true;
-        $window.location.href='/users/' + user.id;
-      }
+        else {
+          localStorage.loggedInUserId = user.id;
+          localStorage.loggedIn = true;
+          $window.location.href='/users/' + user.id;
+        }
 
-    });
+      });
 
   };
 }]);
