8e3e9da6d2ed8c195e57ea642f96fc6ba842e535
John Atkins <johnatkins1999@yahoo.com>
2017-09-01 11:41:24 -1000

    reg/login

--- public/pages/register/register.controller.js
+++ public/pages/register/register.controller.js
@@ -15,27 +15,3 @@ myApp.controller(
     });
   };
 }]);
-angular.module('myApp')
-.controller('RegisterController', ['$scope', 'UsersService', function($scope, UsersService){
-
-  $scope.user = {
-    username: '',
-    password: ''
-  };
-
-  $scope.successMsg = '';
-
-  $scope.addUser = function () {
-
-    console.log('this is the user we are sending from controller', $scope.user);
-
-    UsersService.addUser($scope.user)
-    .then(user => {
-      console.log('came back to controller', user);
-      $scope.user.username = '';
-      $scope.user.password = '';
-      $scope.successMsg = 'Registered!';
-    });
-  }
-}])
-
